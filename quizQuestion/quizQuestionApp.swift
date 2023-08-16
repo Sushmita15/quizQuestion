ac9fee37084dadfc709d61a7ff8c0998d430b5f9
Sushmita Mandal <sushmitamandal100@gmail.com>
2023-08-16 09:54:14 -0400

    Initial Commit

---
+++ quizQuestion/quizQuestionApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizQuestionApp.swift
+//  quizQuestion
+//
+//  Created by scholar on 8/16/23.
+//
+
+import SwiftUI
+
+@main
+struct quizQuestionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
